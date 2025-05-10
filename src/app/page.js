44fb6f07bf9798adba7cbc3a6e9dc448eda36b69
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-05-10 16:13:02 +0330

    created section Hero for home-page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Navbar from '@/components/modules/Navbar'
+import Hero from '@/components/template/home/Hero/Hero'
 import React from 'react'
 
 function Home() {
   return (
     <>
     <Navbar/>
+    <Hero/>
     </>
   )
 }
