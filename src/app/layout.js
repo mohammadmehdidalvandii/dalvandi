341fdfcf1d14da938da3b7dead11f7ecbb820328
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-05-10 14:04:29 +0330

    add metaData is fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,12 @@
 import "./globals.css";
 
 
+
+export const metadata = {
+  title:"My Personal Website",
+  description:"Welcome to my personal website",
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
