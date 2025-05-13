923bf30736738062c58aade49c084bbeab897cfb
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-05-13 15:51:29 +0330

    created page skills

---
+++ src/app/Admin/Skills/page.js
@@ -0,0 +1,17 @@
+import AdminLayout from '@/components/layout/AdminLayout'
+import React from 'react'
+
+export const metadata = {
+  title:"My Personal Website | Dashboard-Skills",
+  description:"Welcome to my personal website",
+}
+
+function page() {
+  return (
+    <AdminLayout>
+        this is skills page
+    </AdminLayout>
+  )
+}
+
+export default page
