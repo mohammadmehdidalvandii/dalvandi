0f10f20479191deb86e1d57ab4c237e542b3ac9c
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-05-17 16:00:43 +0330

    created api created project

---
+++ src/app/api/projects/route.js
@@ -0,0 +1,54 @@
+const connectToDB = require('@/config/db');
+const projectsModel = require('@/models/Projects');
+const {writeFile} = require('fs/promises')
+const path = require('path')
+
+export async function POST (req ){
+
+    try{
+        connectToDB();
+    
+    const formData = await  req.formData();
+    
+    const name = formData.get("name")
+    const description = formData.get("description")
+    const tags = formData.get("tags")
+    const projectURL = formData.get("projectURL")
+    const image = formData.get("image")
+
+    const buffer = Buffer.from(await image.arrayBuffer());
+    const fileName = Date.now() + image.name;
+    const imgPath = path.join(process.cwd() , 'public/uploads/' + fileName)
+    await writeFile(imgPath ,buffer)
+
+    const isExistProject = await projectsModel.findOne({name})
+    if(isExistProject){
+        return Response.json(
+            {message:"Project is Exist database"},
+            {status:422}
+        )
+    }
+
+    const newProject = await projectsModel.create({
+        name ,
+        description,
+        tags ,
+        projectURL ,
+        image:`http://localhost:3000/uploads/${fileName}`
+    });
+
+    if(newProject){
+        return Response.json(
+            {message:"new Projects created " , data:newProject},
+            {status:201},
+        )
+    }
+}
+catch(error){
+    console.log("Error created projects" , error.message);
+      return Response.json(
+            {message:"Error created projects"},
+            {status:500}
+        )
+}
+}
