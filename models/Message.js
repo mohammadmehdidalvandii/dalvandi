dab71cea8793063412661e103ffe753da319c9f2
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-05-18 15:18:08 +0330

    create modelMessage

---
+++ models/Message.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+    name:{
+    type:String,
+    required:true,
+    },
+    email:{
+    type:String,
+    required:true,
+    },
+    subject:{
+    type:String,
+    required:true,
+    },
+    message:{
+    type:String,
+    required:true,
+    }
+},{
+    timestamps:true,
+});
+
+const model = mongoose.models.Message || mongoose.model("Message" , schema);
+module.exports = model;
