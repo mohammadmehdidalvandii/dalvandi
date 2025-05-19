0eaa1a18638aa5ccdc8b3cd7f2b848449065f371
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-05-19 17:05:58 +0330

    created model Activities

---
+++ models/Activities.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+require('./User');
+
+
+const schema = new mongoose.Schema({
+    UserID: {
+        type: mongoose.Types.ObjectId,
+        ref:'User'
+    },
+    data:{
+        type:[String],
+    }
+},{
+    timestamps:true,
+});
+
+const model = mongoose.models.Activities || mongoose.model('Activities', schema)
+
+module.exports = model
