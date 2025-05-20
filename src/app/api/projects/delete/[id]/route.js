cf7d6c9d7ce31deaba63a6de9dc5e27203185562
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-05-20 14:19:28 +0330

    create api projects/delete/id

---
+++ src/app/api/projects/delete/[id]/route.js
@@ -0,0 +1,30 @@
+const connectToDB = require('@/config/db');
+const ProjectModel = require('@/models/Projects');
+
+export async function DELETE (req , {params}){
+    try{
+        connectToDB();
+        const projectID = params?.id;
+
+        if(!projectID){
+            return Response.json(
+                {message :"ProjectID is required"},
+                {status:400}
+            )
+        }else{
+            await ProjectModel.findOneAndDelete({_id:projectID});
+            return Response.json(
+                {message:"Removed ProjectID successfully"},
+                {status:200}
+            )
+        }
+
+    }
+    catch(error){
+        console.log("Delete Project error =>" , error);
+        return Response.json(
+            {message:"Internal server projects delete =>" , error},
+            {status:500}
+        )
+    }
+}
