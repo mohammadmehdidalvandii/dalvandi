2fe5a4dc15ae2021c4e4370cc5f944a0b4002f83
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-05-21 15:17:47 +0330

    add logic logout panel-admin

--- src/components/modules/HeaderAdmin/HeaderAdmin.js
+++ src/components/modules/HeaderAdmin/HeaderAdmin.js
@@ -1,14 +1,34 @@
+"use client"
 import Link from 'next/link'
+import { redirect } from 'next/navigation';
 import React from 'react'
+import swal from 'sweetalert';
 
 function HeaderAdmin() {
+
+  const handlerExit = async (e)=>{
+    e.preventDefault();
+    const res = await fetch('api/auth/signout',{
+      method:"POST"
+    });
+    if(res.status === 200){
+      swal({
+        title:"Logout is successfully",
+        icon:"success",
+        buttons:'Done'
+      }).then(()=>{
+        return redirect('/')
+      })
+    }
+  }
+
   return (
     <section className="bg-white p-3 text-primary rounded-md mb-6">
         <div className="flex justify-center gap-4 sm:justify-between items-center flex-wrap">
             <h5 className="block font-roboto-black text-2xl font-black">Welcome Back Admin</h5>
             <div className="flex items-center gap-4">
                 <Link href='/' className='btn_primary'>Go Home</Link>
-                <button className='btn_primary bg-red-400 hover:bg-red-600 cursor-pointer'>Exit</button>
+                <button className='btn_primary bg-red-400 hover:bg-red-600 cursor-pointer' onClick={handlerExit}>Exit</button>
             </div>
         </div>
     </section>
