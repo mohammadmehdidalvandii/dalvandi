71f69d162b96ee8db5138b828a1c896b909b522d
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-05-25 16:19:54 +0330

    add conent fixed about-page

--- src/app/AboutMe/page.js
+++ src/app/AboutMe/page.js
@@ -3,6 +3,10 @@ import Navbar from '@/components/modules/Navbar/Navbar'
 import About from '@/components/template/aboutme/About/About'
 import React from 'react'
 import { authAdmin } from '@/utils/serverHelper'
+import connectToDB from '@/config/db'
+import ContentModel from '@/models/Content'
+import WorkModel from '@/models/Work'
+import SkillModel from '@/models/Skills'
 
 export const metadata = {
   title:"My Personal Website | About Me",
@@ -10,14 +14,17 @@ export const metadata = {
 }
 
 async function page() {
-
+  await connectToDB()
     const user = await authAdmin()
-  
 
+    const content =  await ContentModel.find({}).exec();
+    const works = await WorkModel.find({}).exec();
+    const skills = await SkillModel.find({}).exec();
+    
   return (
     <>
     <Navbar isAdmin={user?.role}/>
-    <About/>
+    <About about={content[0]?.bioAboutPage} works={JSON.parse(JSON.stringify(works))} skills={JSON.parse(JSON.stringify(skills))}/>
     <Footer/>
     </>
   )
--- src/components/template/aboutme/About/About.js
+++ src/components/template/aboutme/About/About.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 
-function About() {
+function About({about , works , skills}) {
   return (
     <section className="block bg-white py-12 text-primary">
         <div className="container">
@@ -15,26 +15,26 @@ function About() {
                 <div className='col-span-2'>
                     <div className="block">
                         <h5 className="block font-roboto-bold text-2xl text-primary">Who I am</h5>
-                        <p className="block text-text-100 font-roboto-light text-md leading-6 py-4">I am a passionate professional with expertise in various fields. My journey in the industry has equipped me with valuable skills and experiences that I bring to every project.</p>
+                        <p className="block text-text-100 font-roboto-light text-md leading-6 py-4">{about}.</p>
                     </div>
                     <div className="block">
                         <h5 className="block font-roboto-bold text-2xl text-primary ">My Experience</h5>
                         <ul className='list-disc block my-4 *:block *:mb-2 *:font-roboto-bold *:text-md *:text-text-100'>
-                            <li>Collaborated with diverse teams</li>
-                            <li>Collaborated with diverse teams</li>
-                            <li>Collaborated with diverse teams</li>
-                            <li>Collaborated with diverse teams</li>
+                            {works.length > 0  ?(
+                                works.map((work)=>(
+                                    <li key={work._id}>{work.job}-{work.company}-{work.responsibilities}-{work.startDate}-{work.endDate}</li>
+                                ))
+                            ):(
+                                <li>There is no experience work</li>
+                            )}
                         </ul>
                     </div>
                     <div className="block">
                         <h5 className="block font-roboto-bold text-2xl text-primary ">Skills</h5>
                         <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 mt-6">
-                            <div className='block md:w-[220px] font-roboto-bold text-lg text-center py-3 bg-white border-l-3 rounded-sm border-primary drop-shadow-lg duration-300 hover:bg-primary hover:text-white hover:-translate-y-2'>items</div>
-                            <div className='block md:w-[220px] font-roboto-bold text-lg text-center py-3 bg-white border-l-3 rounded-sm border-primary drop-shadow-lg duration-300 hover:bg-primary hover:text-white hover:-translate-y-2'>items</div>
-                            <div className='block md:w-[220px] font-roboto-bold text-lg text-center py-3 bg-white border-l-3 rounded-sm border-primary drop-shadow-lg duration-300 hover:bg-primary hover:text-white hover:-translate-y-2'>items</div>
-                            <div className='block md:w-[220px] font-roboto-bold text-lg text-center py-3 bg-white border-l-3 rounded-sm border-primary drop-shadow-lg duration-300 hover:bg-primary hover:text-white hover:-translate-y-2'>items</div>
-                            <div className='block md:w-[220px] font-roboto-bold text-lg text-center py-3 bg-white border-l-3 rounded-sm border-primary drop-shadow-lg duration-300 hover:bg-primary hover:text-white hover:-translate-y-2'>items</div>
-                            <div className='block md:w-[220px] font-roboto-bold text-lg text-center py-3 bg-white border-l-3 rounded-sm border-primary drop-shadow-lg duration-300 hover:bg-primary hover:text-white hover:-translate-y-2'>items</div>
+                            {skills.map((skill)=>(
+                                <div className='block md:w-[220px] font-roboto-bold text-lg text-center py-3 bg-white border-l-3 rounded-sm border-primary drop-shadow-lg duration-300 hover:bg-primary hover:text-white hover:-translate-y-2' key={skill._id}>{skill.name}</div>
+                            ))}
                         </div>
                     </div>
                 </div>
